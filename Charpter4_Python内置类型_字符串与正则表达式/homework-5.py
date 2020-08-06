58c2612656cd477cbc1d5bc48fb5b177c207a95a
tomtang <tom.tang@me.com>
2020-08-06 17:05:13 +0800

    REIP Homework

--- Charpter4_Python内置类型_字符串与正则表达式/homework-5.py
+++ Charpter4_Python内置类型_字符串与正则表达式/homework-5.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
 """
 --------------------------------------------------------------------------------
 接口      : Port-channel1.189
@@ -6,11 +8,19 @@ IP地址    : 192.168.189.254
 """
 
 import re
-
+a = '接口'
+b = 'IP地址'
+c = 'status'
 str1 = 'Port-channel1.189   192.168.189.254 YES CONFIG  up  up '
 
 #po = re.match('\D+\d+((/\d+)+(\.\d+)?)?', str1)
-po = re.match('\D+[-]?\d+[.]?\d+',str1)
-ip = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',str1)
+po = re.match(r'(\D+[-]?\d+[.]?\d+)\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+(\w*)\s+(\w*)\s+(\w*)\s+(\w*)\s*',str1).groups()
+#ip = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}',str1)
+#status = re.search(('up'), str1)
+print(po)
+print('-'*80)
+print(f'{a:<7}: {po[0]:<10}')
+print(f'{b:<7}: {po[1]:<10}')
+print(f'{c:<8}: {po[-1]:<10}')
 
-print(ip)
+
